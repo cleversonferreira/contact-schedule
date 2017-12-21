fa1569730a5ab96626b655df932e3ccbd00b0220
Cleverson <cleverson_reisferreira@hotmail.com>
2017-12-21 17:34:57 -0200

    implementing findOne and findAll method

--- app/routes/schedule_routes.js
+++ app/routes/schedule_routes.js
@@ -1,3 +1,5 @@
+let ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
 
   //home
@@ -5,9 +7,30 @@ module.exports = function(app, db) {
     res.send('Contact Schedule API')
   });
 
+  //findAll
+  app.get('/all', (req, res) => {
+    db.collection('contact').find({}).toArray(function(err, data) {
+      if (err) console.log(err);
+      res.send(data);
+    });
+  });
+
+  //findOne
+  app.get('/contact/:id', (req, res) => {
+    let id = req.params.id;
+    let details = { '_id': new ObjectID(id) };
+    db.collection('contact').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
   //new contact
   app.post('/contact', (req, res) => {
-    const contact = { name: req.body.name, last_name: req.body.last_name, email: req.body.email, phone: req.body.phone };
+    let contact = { name: req.body.name, last_name: req.body.last_name, email: req.body.email, phone: req.body.phone };
     db.collection('contact').insert(contact, (err, result) => {
       if (err) return res.send("error" + err);
         res.send(result.ops[0]);
